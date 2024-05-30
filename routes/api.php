1921242ae5c4d3b0b423ba3f87ed9594ad94ddcc
Pham-Gia-Bao1 <134178887+Pham-Gia-Bao1@users.noreply.github.com>
2024-05-30 10:27:38 +0700

    Feature/v npay (#55)
    * text
    
    * write VNpay api

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FeedbackController;
 use App\Models\Feedback;
+use App\Http\Controllers\PaymentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -135,6 +136,9 @@ Route::prefix('posts')->group(function () {
 //contact us
 // create new contact
 Route::post('/contactUs', [ContactController::class, 'contactUs']);
+
+//VN pay
+Route::post('/payment', [PaymentController::class, 'makePayment']);
 
 
 // auth api
