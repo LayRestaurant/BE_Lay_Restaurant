a519653c12efe01253383b4076ed664477188138
Bquyen0508 <143238500+Bquyen0508@users.noreply.github.com>
2024-06-04 22:40:04 +0700

    Bugfix/get expert detail controller (#63)
    * finish changes
    
    * fix get feedback experts

--- app/Http/Controllers/ExpertDetailController.php
+++ app/Http/Controllers/ExpertDetailController.php
@@ -86,12 +86,12 @@ class ExpertDetailController extends Controller
             ->get();
 
         $feedback = DB::table('bookings')
+            ->join('users', 'users.id', '=', 'bookings.user_id')
             ->join('feedback_experts', 'bookings.id', '=', 'feedback_experts.booking_id')
-            ->join('calendars', 'calendars.id', '=', 'bookings.calendar_id') // Corrected the join condition
+            ->join('calendars', 'calendars.id', '=', 'bookings.calendar_id')
             ->where('calendars.expert_id', '=', $id)
-            ->select('feedback_experts.*')
+            ->select('feedback_experts.*', 'users.name', 'users.profile_picture')
             ->get();
-
 
         // Kết hợp thông tin từ $user và $expertDetail vào một mảng
         $data = [
