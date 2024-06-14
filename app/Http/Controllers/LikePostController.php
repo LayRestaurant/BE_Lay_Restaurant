a619affdf212c25885607649224ca9302f000de9
Việt Mỹ <137087064+ZietMy@users.noreply.github.com>
2024-06-14 23:08:16 +0700

    Bug fix expert (#87)
    * fix
    
    * fix-api-getExpertDetail
    
    ---------
    
    Co-authored-by: hiep.bui <hiep.bui@messagemedia.com>

--- app/Http/Controllers/LikePostController.php
+++ app/Http/Controllers/LikePostController.php
@@ -52,13 +52,9 @@ class LikePostController extends Controller
         try {
             $user = $this->getUser($request);
             $likePost = LikePost::where('user_id', $user->id)->where('post_id', $postId)->first();
+            $likePost->delete();
 
-            if ($likePost) {
-                $likePost->delete();
-                return response()->json(['success' => true, 'message' => 'Post unliked successfully'], 200);
-            }
-
-            return response()->json(['success' => false, 'message' => 'Like not found'], 404);
+            return response()->json(['success' => true], 200);
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
