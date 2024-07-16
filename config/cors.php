5576c4e3887133ce9d75b0f7619cfe1555c28d5b
Bao Pham <bao.pham@sapotacorp.vn>
2024-07-16 22:22:41 +0700

    fix cors error to alowing the access from FE

--- config/cors.php
+++ config/cors.php
@@ -1,42 +1,12 @@
 <?php
-
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-    'paths' => ['api/*', 'sanctum/csrf-cookie', '/csrf-token'],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    // Add multiple domains here
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:3000'),
-        'http://localhost:3001',
-        'http://localhost:3002',
-        'http://localhost:3003',
-        'https://bitstorm.zeabur.app',
-        'https://admin-bitstorm.zeabur.app',
-        'https://lay-restaurant.vercel.app/'
-
-    ],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    'supports_credentials' => true,
-
+    'supports_credentials' => false,
 ];
+
