79617d104f3ec4030a32f8f5fcf59756a1e1269f
Bao Pham <bao.pham@sapotacorp.vn>
2024-07-16 22:49:25 +0700

    fixed cors error

--- config/cors.php
+++ config/cors.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie','/csrf-token'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'api/csrf-token'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
