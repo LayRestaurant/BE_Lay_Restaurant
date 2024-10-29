3d7630a5b47ca68b297dbdc98ce0abcfa976bc74
Bao Pham <bao.pham@sapotacorp.vn>
2024-10-29 10:04:58 +0700

    completed update booking room

--- database/factories/BookingRoomFactory.php
+++ database/factories/BookingRoomFactory.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Database\Factories;
 
 use App\Models\BookingRoom;
@@ -15,15 +16,16 @@ class BookingRoomFactory extends Factory
         $checkOutDate = (clone $checkInDate)->modify("+{$numberOfDays} days");
 
         return [
-            'user_id' => $this->faker->numberBetween(1, 10),
-            'room_id' => $this->faker->numberBetween(1, 15),
-            'check_in_date' => $checkInDate->format('Y-m-d H:i:s'),
-            'check_out_date' => $checkOutDate->format('Y-m-d H:i:s'),
-            'price' => $this->faker->randomFloat(2, 50, 500),
-            'status' => 'booked',
-            'payment_status' => $this->faker->boolean,
-            'number_of_days' => $numberOfDays,
+            'guest_id' => $this->faker->numberBetween(1, 10), // Thêm guest_id
+            'room_id' => $this->faker->numberBetween(1, 15), // Thêm room_id
+            'check_in_date' => $checkInDate->format('Y-m-d H:i:s'), // Ngày nhận phòng
+            'check_out_date' => $checkOutDate->format('Y-m-d H:i:s'), // Ngày trả phòng
+            'number_of_days' => $numberOfDays, // Số ngày ở
+            'number_of_guests' => $this->faker->numberBetween(1, 4), // Số lượng khách
+            'total_price' => $this->faker->randomFloat(2, 50, 500), // Tổng giá cho booking
+            'status' => 'booked', // Trạng thái booking
+            'payment_status' => $this->faker->boolean, // Trạng thái thanh toán
+            'notes' => $this->faker->sentence(), // Các ghi chú bổ sung
         ];
     }
 }
-
